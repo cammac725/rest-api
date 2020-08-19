e9aa786586da0e90a8abd74a229d6c52de587cab
cammac <camm@rocktechcomputer.com>
2020-08-19 13:22:04 -0700

    finished and tested auth

--- routes/main.js
+++ routes/main.js
@@ -18,12 +18,20 @@ router.post(
   res.status(200).json({ message: "signup successful!", status: 200 });
 });
 
-router.post("/login", (req, res) => {
-  if (!req.body) {
-    res.status(400).json({ message: "Invalid body", status: 400 });
-  } else {
-    res.status(200).json({ message: "Posted!", status: 200 });
-  }
+router.post("/login", (req, res, next) => {
+  passport.authenticate("login", (error, user) => {
+    try {
+      if(error) return next(error);
+      if (!user) return next(new Error('email and password are required'));
+      req.login(user, { session: false }, (err) => {
+        if (error) return next(error);
+        return res.status(200).json({ user, status: 200 });
+      });
+    } catch (err) {
+      console.log(err);
+      return next(err)
+    }
+  })(req, res, next);
 });
 
 router.post("/logout", (req, res) => {
