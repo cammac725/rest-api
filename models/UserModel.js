4576f2b90b186debb369fb2a73a21a7fdee6cb46
cammac <camm@rocktechcomputer.com>
2020-09-11 16:41:03 -0700

    add bcrypt

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const { Schema } = mongoose;
 
 const UserSchema = new Schema({
@@ -22,6 +24,18 @@ const UserSchema = new Schema({
     type: Date,
   },
 });
+
+UserSchema.pre('save', async function(next) {
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function(password) {
+  const user = this;
+  const compare = await bcrypt.compare(password, user.password);
+  return compare;
+};
 
 const UserModel = mongoose.model('user', UserSchema);
 
