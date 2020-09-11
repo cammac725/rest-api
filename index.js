b2a2421da373fde171305d8b2202c5ce69cfa021
cammac <camm@rocktechcomputer.com>
2020-09-11 16:28:06 -0700

    install mongo, mongoose, setup models

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 
 const routes = require('./routes/main');
 const passwordRoutes = require('./routes/password')
 
-const app = express();
+// setup mongo connection
+const uri = process.env.MONGO_CONNECTION_URL;
+const mongoConfig = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+};
+if (process.env.MONGO_USER_NAME && process.env.MONGO_PASSWORD) {
+  mongoConfig.auth = { authSource: 'admin' };
+  mongoConfig.user = process.env.MONGO_USER_NAME;
+  mongoConfig.pass = process.env.MONGO_PASSWORD;
+}
+mongoose.connect(uri, mongoConfig);
 
-const port = process.env.PORT || 80;
-app.listen(port, () => {
-  console.log(`server is running on port: ${port}`)
+// listen for the event (error)
+mongoose.connection.on('error', error => {
+  console.log(error);
+  process.exit(1);
 });
+
+const app = express();
+const port = process.env.PORT || 8000;
 
 // update express settings
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -36,4 +53,12 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   console.log(err);
   res.status(err.status || 500).json({ err: err.message, status: 500 });
+});
+
+// connect to the database, THEN start the server 
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo");
+  app.listen(port, () => {
+    console.log(`server is running on port: ${port}`);
+  });
 });
