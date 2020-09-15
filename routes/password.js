42215a6422e62494631ac39c8177b654c44ba92a
cammac <camm@rocktechcomputer.com>
2020-09-15 10:18:14 -0700

    install nodemailer, add to password.js

--- routes/password.js
+++ routes/password.js
@@ -1,4 +1,35 @@
 const express = require("express");
+const hbs = require('nodemailer-express-handlebars');
+const nodemailer = require('nodemailer');
+const path = require('path');
+
+// create variables for email and password (in .env file)
+const email = process.env.EMAIL;
+const password = process.env.PASSWORD;
+
+// create transport object, inform nodemailer how to send emails
+const smtpTransport = nodemailer.createTransport({
+  service: process.env.EMAIL_PROVIDER,
+  auth: {
+    user: email,
+    pass: password,
+  },
+});
+
+// create handlebars options
+const handlebarsOptions = {
+  // what templating engine to use
+  viewEngine: {
+    extName: '.hbs',
+    partialsDir: './templates/',
+    layoutsDir: './templates/',
+  },
+  viewPath: path.resolve('./templates/'),
+  extName: '.html',
+};
+
+smtpTransport.use('compile', hbs(handlebarsOptions));
+
 const router = express.Router();
 
 router.post("/forgot-pw", (req, res) => {
